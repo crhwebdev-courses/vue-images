a1839acecf29bded322ef647a97052964cc6dc17
Carl Hain <crhain@gmail.com>
2019-11-12 15:06:19 -0600

    Wire up auth module to vuex instance

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import Vuex from "vuex";
 import Vue from "vue";
+import auth from "./modules/auth";
 
 //connect Vuex to Vue using... it's maaagic.
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {}
+  modules: {
+    auth
+  }
 });
