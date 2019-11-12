9f86686d04f0e9aebb3523bb18e3004d15ac3a4e
Carl Hain <crhain@gmail.com>
2019-11-12 10:08:53 -0600

    Add AppHeader items and style it using semantic-ui

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>Image Storage</div>
+  <div class="ui secondary pointing menu">
+    <a href="/" class="active item">
+      Image Storage
+    </a>
+    <div class="right menu">
+      <a href="" class="ui item">
+        Login
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
