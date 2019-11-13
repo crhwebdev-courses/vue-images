6aa2df64ec18e602e0f23b9cd1edffc055972ce9
Carl Hain <crhain@gmail.com>
2019-11-13 09:15:46 -0600

    Configure VueRouter to use a BrowserRouter instead of a HashRouter

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import store from "./store/";
 //Add VueRouter as middleware
 Vue.use(VueRouter);
 
-//initialize VueRouter with routes we are using
+//initialize VueRouter with routes we are using and set mode to 'history'
+// which means to use a BrowserRouter instead of HashRouter
 const router = new VueRouter({
+  mode: "history",
   routes: [{ path: "/oauth2/callback", component: AuthHandler }]
 });
 
