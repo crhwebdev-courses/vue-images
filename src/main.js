cf17d6d137fe9a4f8c2e417d2287cb9773f9ad6a
Carl Hain <crhain@gmail.com>
2019-11-13 09:03:06 -0600

    Wire up VueRouter in main.js -- still need to create AuthHandler

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App";
 import store from "./store/";
 
+//Add VueRouter as middleware
+Vue.use(VueRouter);
+
+//initialize VueRouter with routes we are using
+const router = new VueRouter({
+  routes: [{ path: "/oauth2/callback", component: AuthHandler }]
+});
+
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount("#app");
