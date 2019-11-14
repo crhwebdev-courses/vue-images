fdcae637d69c1f86747438b058a299d47d4c3edb
Carl Hain <crhain@gmail.com>
2019-11-14 12:05:34 -0600

    ImageList now prints a title, desription, and image for each image fetched from imgur

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>Image List</div>
+  <div>
+    <div v-for="image in allImages" :allImages="allImages">
+      <h4>{{ image.title }}</h4>
+      <p>{{ image.description }}</p>
+      <img :src="image.link" :alt="image.title" />
+    </div>
+  </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 export default {
   name: "ImageList",
   methods: {
     ...mapActions(["fetchImages"])
+  },
+  computed: {
+    ...mapGetters(["allImages"])
   },
   created() {
     // Vue Lifecycle method that gets called after a component
